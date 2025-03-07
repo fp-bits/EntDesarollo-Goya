9eb33834a911cf5ae4d21457e8d0f09d38fabba9
xGaLaXydev <israelroyanooro@gmail.com>
2025-03-07 11:36:59 +0100

    Proyecto colaborativo con Alex

---
+++ AlexEIsra/src/colaborativo/Mueble.java
@@ -0,0 +1,26 @@
+package colaborativo;
+
+public class Mueble {
+	private String material;
+	private int anyos;
+	
+	public Mueble(String material, int anyos) {
+		super();
+		this.material = material;
+		this.anyos = anyos;
+	}
+	public String getMaterial() {
+		return material;
+	}
+	public void setMaterial(String material) {
+		this.material = material;
+	}
+	public int getAnyos() {
+		return anyos;
+	}
+	public void setAnyos(int anyos) {
+		this.anyos = anyos;
+	}
+	
+	
+}
---
+++ AlexEIsra/src/colaborativo/UsoMueble.java
@@ -0,0 +1,10 @@
+package colaborativo;
+
+public class UsoMueble {
+
+	public static void main(String[] args) {
+		Mueble m=new Mueble("madera",4);
+		System.out.println(m);
+	}
+
+}
